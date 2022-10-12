1ec37614513d43aa7b8d036a810fe1f58a2a59c9
DyingZhang <1535661945@qq.com>
2022-10-12 10:32:34 +0800

    backend-init-project

---
+++ electronic-case-backend-parent/src/main/java/com/zdy/electroniccasebackendparent/ElectronicCaseBackendParentApplication.java
@@ -0,0 +1,13 @@
+package com.zdy.electroniccasebackendparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicCaseBackendParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicCaseBackendParentApplication.class, args);
+    }
+
+}
